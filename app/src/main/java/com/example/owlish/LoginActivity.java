af59f64c761b2e39ac11c22591a202cf1d8c6871
nithinjoy <56895565+nithin-j@users.noreply.github.com>
2020-02-19 20:25:45 -0500

    Login UI

---
+++ app/src/main/java/com/example/owlish/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.owlish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void LoadRegister(View v){
+
+        //startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+    }
+}
