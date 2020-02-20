36ccad1af54a63c64bda0c39962255b29823e88a
nithinjoy <56895565+nithin-j@users.noreply.github.com>
2020-02-19 20:48:06 -0500

    Registration UI

--- app/src/main/java/com/example/owlish/RegistrationActivity.java
+++ app/src/main/java/com/example/owlish/RegistrationActivity.java
@@ -6,16 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
     }
 
-    public void LoadRegister(View v){
-
-        startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+    public void LoadLogin(View v){
+        startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
     }
 }
